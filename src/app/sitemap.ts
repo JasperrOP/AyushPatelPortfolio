6a58c4283ab375ff75c5f31e38f84a1336950a38
JasperrOP <your-github-email@example.com>
2026-07-12 17:03:33 +0530

    Add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://ayushpatel3.vercel.app/";
+  const baseUrl = "https://ayushpatel11.vercel.app/";
 
   return [
     {
